66b4dc9a9c2e832d8e51f97e93561aa5af617699
abelgeorgeantony <abelgeorgeantony@gmail.com>
2023-09-23 20:21:53 +0530

    Initial commit

---
+++ colorpicker.js
@@ -0,0 +1,15 @@
+function colorpicker(row,col) {
+    if((row === 0) && (col ===0))
+    {
+        close();
+        return(0);
+    }
+    open();
+}
+
+function open() {
+
+}
+function close() {
+    
+}
